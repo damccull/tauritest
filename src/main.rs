e15ee94ee0df95ac163006adfebe547c5c591108
damccull <damccull+git@gmail.com>
2022-02-18 12:49:39 -0500

    Other tutorial complete

--- src/main.rs
+++ src/main.rs
@@ -3,101 +3,44 @@ use serde::Deserialize;
 use yew::prelude::*;
 
 fn main() {
-    yew::start_app::<App>();
+    yew::start_app::<Model>();
 }
 
-#[function_component(App)]
-fn app() -> Html {
-    let videos = use_state(std::vec::Vec::new);
-    {
-        let videos = videos.clone();
-        use_effect_with_deps(
-            move |_| {
-                let videos = videos;
-                wasm_bindgen_futures::spawn_local(async move {
-                    let fetched_videos: Vec<Video> = Request::get("/tutorial/data.json")
-                        .send()
-                        .await
-                        .unwrap()
-                        .json()
-                        .await
-                        .unwrap();
-                    videos.set(fetched_videos);
-                });
-                || ()
-            },
-            (),
-        );
+enum Msg {
+    AddOne,
+}
+
+struct Model {
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { value: 0 }
     }
 
-    let selected_video = use_state(|| None);
-    let on_video_select = {
-        let selected_video = selected_video.clone();
-        Callback::from(move |video: Video| selected_video.set(Some(video)))
-    };
-
-    let details = selected_video.as_ref().map(|video| {
-        html! {
-            <VideoDetails video={video.clone()} />
-        }
-    });
-
-    html! {
-        <>
-            <h1> { "RustConf Explorer" }</h1>
-            <div>
-                <h3>{ "Videos to watch" }</h3>
-                <VideosList videos={(*videos).clone()} on_click={on_video_select.clone()} />
-            </div>
-            { for details }
-        </>
-    }
-}
-
-#[derive(Clone, Deserialize, PartialEq)]
-struct Video {
-    id: usize,
-    title: String,
-    speaker: String,
-    url: String,
-}
-
-#[derive(Properties, PartialEq)]
-struct VideosListProps {
-    videos: Vec<Video>,
-    on_click: Callback<Video>,
-}
-
-#[function_component(VideosList)]
-fn videos_list(VideosListProps { videos, on_click }: &VideosListProps) -> Html {
-    let on_click = on_click.clone();
-    videos
-        .iter()
-        .map(|video| {
-            let on_video_select = {
-                let on_click = on_click.clone();
-                let video = video.clone();
-                Callback::from(move |_| on_click.emit(video.clone()))
-            };
-
-            html! {
-                <p class="video-entry" onclick={on_video_select}>{format!("{}: {}", video.speaker, video.title)}</p>
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                // The value has changed so re-render to make it appear on the page
+                true
             }
-        })
-        .collect::<Html>()
-}
+        }
+    }
 
-#[derive(Clone, Properties, PartialEq)]
-struct VideoDetailsProps {
-    video: Video,
-}
-
-#[function_component(VideoDetails)]
-fn video_details(VideoDetailsProps { video }: &VideoDetailsProps) -> Html {
-    html! {
-        <div>
-            <h3>{ video.title.clone() }</h3>
-            <img src="https://via.placeholder.com/640x360.png?text=Video+Plaer+Placeholder" alt="video thumbnail" />
-        </div>
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        // This gives us a component's "`Scope`" which allows us to send messages
+        // and stuff to the component.
+        let link = ctx.link();
+        html! {
+            <div>
+                <button onclick={link.callback(|_| Msg::AddOne)}> { "+1" }</button>
+                <p>{ self.value }</p>
+            </div>
+        }
     }
 }
