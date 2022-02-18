5a19e6c6d213b9a636c417ae250c16b0e0144175
damccull <damccull+git@gmail.com>
2022-02-17 22:05:58 -0500

    Completed tutorial.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,103 @@
+use reqwasm::http::Request;
+use serde::Deserialize;
+use yew::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    yew::start_app::<App>();
+}
+
+#[function_component(App)]
+fn app() -> Html {
+    let videos = use_state(std::vec::Vec::new);
+    {
+        let videos = videos.clone();
+        use_effect_with_deps(
+            move |_| {
+                let videos = videos;
+                wasm_bindgen_futures::spawn_local(async move {
+                    let fetched_videos: Vec<Video> = Request::get("/tutorial/data.json")
+                        .send()
+                        .await
+                        .unwrap()
+                        .json()
+                        .await
+                        .unwrap();
+                    videos.set(fetched_videos);
+                });
+                || ()
+            },
+            (),
+        );
+    }
+
+    let selected_video = use_state(|| None);
+    let on_video_select = {
+        let selected_video = selected_video.clone();
+        Callback::from(move |video: Video| selected_video.set(Some(video)))
+    };
+
+    let details = selected_video.as_ref().map(|video| {
+        html! {
+            <VideoDetails video={video.clone()} />
+        }
+    });
+
+    html! {
+        <>
+            <h1> { "RustConf Explorer" }</h1>
+            <div>
+                <h3>{ "Videos to watch" }</h3>
+                <VideosList videos={(*videos).clone()} on_click={on_video_select.clone()} />
+            </div>
+            { for details }
+        </>
+    }
+}
+
+#[derive(Clone, Deserialize, PartialEq)]
+struct Video {
+    id: usize,
+    title: String,
+    speaker: String,
+    url: String,
+}
+
+#[derive(Properties, PartialEq)]
+struct VideosListProps {
+    videos: Vec<Video>,
+    on_click: Callback<Video>,
+}
+
+#[function_component(VideosList)]
+fn videos_list(VideosListProps { videos, on_click }: &VideosListProps) -> Html {
+    let on_click = on_click.clone();
+    videos
+        .iter()
+        .map(|video| {
+            let on_video_select = {
+                let on_click = on_click.clone();
+                let video = video.clone();
+                Callback::from(move |_| on_click.emit(video.clone()))
+            };
+
+            html! {
+                <p class="video-entry" onclick={on_video_select}>{format!("{}: {}", video.speaker, video.title)}</p>
+            }
+        })
+        .collect::<Html>()
+}
+
+#[derive(Clone, Properties, PartialEq)]
+struct VideoDetailsProps {
+    video: Video,
+}
+
+#[function_component(VideoDetails)]
+fn video_details(VideoDetailsProps { video }: &VideoDetailsProps) -> Html {
+    html! {
+        <div>
+            <h3>{ video.title.clone() }</h3>
+            <img src="https://via.placeholder.com/640x360.png?text=Video+Plaer+Placeholder" alt="video thumbnail" />
+        </div>
+    }
 }
