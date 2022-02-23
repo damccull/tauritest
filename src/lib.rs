5d699d14e0f8f8ea0b6904ea2e8e7cee8b281aae
damccull <401104+damccull@users.noreply.github.com>
2022-02-23 00:59:34 -0500

    Additional playing

--- src/lib.rs
+++ src/lib.rs
@@ -47,25 +47,34 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
 
 // `view` describes what to display.
 fn view(model: &Model) -> Node<Msg> {
+    let imgval = format!(
+        "https://via.placeholder.com/250/AAAAAA/000000?text=Clicks+{}",
+        model.counter
+    );
     div![
-        C!["p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4"],
-        div![
-            C!["shrink-0"],
-            div![
-                C!["h-12 w-12 text-[1.5em] border text-center"],
-                p![C!["inline-block align-middle"], model.counter],
-            ],
+        C!["py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6"],
+        img![
+            attrs!{At::Src => imgval},
+            C!["block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0"],
         ],
         div![
-            div![C!["font-serif"], "Counter"],
+            C!["text-center space-y-2 sm:text-left"],
+            div![C!["space-y-0.5"],
+                p!["Counter", C!["text-lg text-black font-semibold"]],
+                p!["Increase Counter", C!["text-slate-500 font-medium"]],
+            ],
             button![
                 "Click me!",
                 ev(Ev::Click, |_| Msg::Increment),
-                C!["button"],
                 attrs! {At::Type=>"button"},
+                C!["px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border"],
+                C!["border-purple-200 hover:text-white hover:bg-purple-600"],
+                C!["hover:border-transparent focus:outline-none focus:ring-2"],
+                C!["focus:ring-purple-600 focus:ring-offset-2"],
             ],
-        ]
+        ],
     ]
+
     // div![
     //     "This is a counter: ",
     //     C!["counter"],
